48413b3b2e25a1e0900a0eb1193b1312bb229c08
shuto-shinoda <shuto.88.tb@gmail.com>
2021-02-28 18:06:18 +0900

    計算

--- study.js
+++ study.js
@@ -19,3 +19,26 @@ var randnum = parseInt(Math.random() * 10) + 1;  //小数点以下切り捨てpa
 //console.log(randnum);
 //var number = 100;
 console.log("スライムが" + randnum + "匹あらわれた")
+
+// 計算する
+var number = (100 + 2) * 3;
+console.log(number + number);
+console.log(number)
+
+// 値段を計算する
+var apple_price = (parseInt(Math.random() * 10) + 1) * 100;  //リンゴの単価
+var apple_num = parseInt(Math.random() * 10) + 1;  //リンゴを買う数
+console.log("リンゴの単価 :" + apple_price + "円");
+console.log("リンゴを買う数 :" + apple_num +"値");
+var total = apple_price * apple_num;
+console.log("合計金額 :" +total + "円");
+
+// データの種類
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var number = 100 + 40;// 数値
+var text = "ハロー" + "paiza";// 文字列
+console.log(number);
+console.log(text);
+console.log(number + text);
+console.log(number + 20);
